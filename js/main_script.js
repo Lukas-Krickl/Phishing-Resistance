d7dd08e4b39ebfc4696040ecd43c83bf061e1139
Lukas <lukas.krickl@outlook.at>
2020-04-08 14:33:30 +0200

    fixed iphone mobile issue

--- js/main_script.js
+++ js/main_script.js
@@ -4,3 +4,10 @@ const mobileNavContainer = document.getElementById('navLinks');
 function toggleMobileNav(show) {
   mobileNavContainer.classList.toggle("nav-closed", !show);
 }
+
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
